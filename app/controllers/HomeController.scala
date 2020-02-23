80ebe03648c870bd06ecbe78c349aaf11471e59e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-24 05:03:46 +0900

    Creating a view
    viewの作成

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,6 +5,7 @@ import play.api._
 import play.api.mvc._
 import akka.util._
 import play.api.http._
+import java.util._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -20,6 +21,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
+
   //  クッキーの利用
   // def index(name:Option[String]) = Action{request =>
   //   val param:String = name.getOrElse("");
@@ -37,18 +39,24 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   //     res
   //   }
   // }
+
   // セッションの利用
-  def index(name:Option[String], value:Option[String]) = Action{request =>
-    val s_name:String = name.getOrElse("");
-    val s_value:String = value.getOrElse("");
-    val sessions = request.session.data
-    val message = "<pre>" + sessions + "</pre>"
-    val res = Ok("<tittle>Hello!</tittle><h1>Hello!</h1>" + message)
-    .as("text/html")
-    if (name != ""){
-      res.withSession(request.session + (s_name -> s_value))
-    }else{
-      res
-    }
+  // def index(name:Option[String], value:Option[String]) = Action{request =>
+  //   val s_name:String = name.getOrElse("");
+  //   val s_value:String = value.getOrElse("");
+  //   val sessions = request.session.data
+  //   val message = "<pre>" + sessions + "</pre>"
+  //   val res = Ok("<tittle>Hello!</tittle><h1>Hello!</h1>" + message)
+  //   .as("text/html")
+  //   if (name != ""){
+  //     res.withSession(request.session + (s_name -> s_value))
+  //   }else{
+  //     res
+  //   }
+  // }
+
+  // viewを使用
+  def index(p:Option[Int])= Action{
+    Ok(views.html.index("これはコントローラーで用意したメッセージです。", p.getOrElse(0)))
   }
 }
