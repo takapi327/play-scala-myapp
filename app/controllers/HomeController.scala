b1db29500b358d1e558b93a4188cf42a73f23f3e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-24 02:21:26 +0900

    Practice the controller
    コントローラの練習

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,8 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import akka.util._
+import play.api.http._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -18,7 +20,35 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  //  クッキーの利用
+  // def index(name:Option[String]) = Action{request =>
+  //   val param:String = name.getOrElse("");
+  //   var message = "<p>nameはありません。</p>"
+  //   if (param != ""){
+  //     message = "<p>nameが送られました。</p>"
+  //   }
+  //   val cookie = request.cookies.get("name")
+  //   message += "<p>cookie: " + cookie.getOrElse(Cookie("name", "no-cookie.")).value + "</p>"
+  //   val res = Ok("<tittle>Hello!</tittle><h1>Hello!</h1>" + message)
+  //   .as(HTML)
+  //   if (param != ""){
+  //     res.withCookies(Cookie("name", param)).bakeCookies()
+  //   }else{
+  //     res
+  //   }
+  // }
+  // セッションの利用
+  def index(name:Option[String], value:Option[String]) = Action{request =>
+    val s_name:String = name.getOrElse("");
+    val s_value:String = value.getOrElse("");
+    val sessions = request.session.data
+    val message = "<pre>" + sessions + "</pre>"
+    val res = Ok("<tittle>Hello!</tittle><h1>Hello!</h1>" + message)
+    .as("text/html")
+    if (name != ""){
+      res.withSession(request.session + (s_name -> s_value))
+    }else{
+      res
+    }
   }
 }
