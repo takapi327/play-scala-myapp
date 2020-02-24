9bc74edc5c901f2122ad71dfc4165daef3230d05
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-25 01:27:30 +0900

    Create create action
    createアクションの作成

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,6 +7,7 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.db._
+import PersonForm._
 
 @Singleton
 class HomeController @Inject()(db: Database, cc: MessagesControllerComponents) extends MessagesAbstractController(cc){
@@ -27,5 +28,30 @@ class HomeController @Inject()(db: Database, cc: MessagesControllerComponents) e
       msg = "<li>no record...</li>"
     }
     Ok(views.html.index(msg))
+  }
+
+  def add() = Action{implicit request =>
+    Ok(views.html.add("フォームを記入して下さい。", form))
+  }
+
+  def create() = Action{implicit request =>
+    val formdata = form.bindFromRequest
+    val data = formdata.get
+    try
+      db.withConnection{conn =>
+        val ps = conn.prepareStatement(
+          "insert into people values (default, ?, ?, ?)")
+        ps.setString(1, data.name)
+        ps.setString(2, data.mail)
+        ps.setString(3, data.tel)
+        ps.executeUpdate
+      }
+    catch{
+      case e: SQLException =>
+        Ok(views.html.add(
+          "フォームを入力してください。", form
+        ))
+    }
+    Redirect(routes.HomeController.index)
   }
 }
