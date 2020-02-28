1beb4820be75e06a379850c95f3744f57f093712
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-28 13:23:59 +0900

    Edit SQL CRUD using Anorm
    Anormを使いSQLのCRUDを編集

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -13,26 +13,11 @@ import anorm._
 @Singleton
 class HomeController @Inject()(db: Database, cc: MessagesControllerComponents) extends MessagesAbstractController(cc){
 
-  def index() = Action{implicit request =>
-    db.withConnection{implicit conn =>
-      val result:List[String] = SQL("Select * from people").as(SqlParser.str("name"). *)
+  def index() = Action {implicit request =>
+    db.withConnection {implicit conn =>
+      val result:List[PersonData] = SQL("Select * from people").as(personparser. *)
       Ok(views.html.index("People Data.", result))
     }
-    // var msg = "database record:<br><ul>"
-    // try{
-    //   db.withConnection{conn =>
-    //   val stmt = conn.createStatement
-    //   val rs = stmt.executeQuery("SELECT * from people")
-    //   while(rs.next){
-    //     msg += "<li>" + rs.getInt("id") + ":" + rs.getString("name") + "</li>"
-    //   }
-    //   msg += "</ul>"
-    //   }
-    // } catch{
-    //   case e:SQLException =>
-    //   msg = "<li>no record...</li>"
-    // }
-    // Ok(views.html.index(msg))
   }
 
   def add() = Action{implicit request =>
@@ -42,93 +27,60 @@ class HomeController @Inject()(db: Database, cc: MessagesControllerComponents) e
   def create() = Action{implicit request =>
     val formdata = form.bindFromRequest
     val data = formdata.get
-    try
-      db.withConnection{conn =>
-        val ps = conn.prepareStatement(
-          "insert into people values (default, ?, ?, ?)")
-        ps.setString(1, data.name)
-        ps.setString(2, data.mail)
-        ps.setString(3, data.tel)
-        ps.executeUpdate
-      }
-    catch{
-      case e: SQLException =>
-        Ok(views.html.add(
-          "フォームを入力してください。", form
-        ))
+    db.withConnection { implicit conn =>
+      SQL("insert into people values (default, {name}, {mail}, {tel})")
+      .on(
+        "name" -> data.name, 
+        "mail" -> data.mail,
+        "tel" -> data.tel
+        ).executeInsert()
+      Redirect(routes.HomeController.index)
     }
-    Redirect(routes.HomeController.index)
   }
 
   def edit(id:Int) = Action{implicit request =>
-    var formdata = form.bindFromRequest
-    try{
-      db.withConnection{conn =>
-        val stmt = conn.createStatement
-        val rs = stmt.executeQuery("selest * from people where id=" + id)
-        rs.next
-        val name = rs.getString("name")
-        val mail = rs.getString("mail")
-        val tel = rs.getString("tel")
-        val data = Data(name, mail, tel)
-        formdata = form.fill(data)
-      }
-    }catch{
-      case e:SQLException =>
-        Redirect(routes.HomeController.index)
+    var formdata = personform.bindFromRequest
+    db.withConnection { implicit conn =>
+      val pdata:PersonData = SQL("select * from people where id={id}").on("id" -> id).as(personparser.single)
+      formdata = personform.fill(pdata)
+      Ok(views.html.edit("フォームを編集して下さい。", formdata, id))
     }
-    Ok(views.html.edit("フォームを編集して下さい。", formdata, id))
   }
 
   def update(id:Int) = Action{implicit request =>
     val formdata = form.bindFromRequest
     val data = formdata.get
-    try
-      db.withConnection{conn =>
-        val ps = conn.prepareStatement("update people set name=?, mail=?, tel=? where id=?")
-        ps.setString(1, data.name)
-        ps.setString(2, data.mail)
-        ps.setString(3, data.tel)
-        ps.setInt(4, id)
-        ps.executeUpdate
-      }
-    catch{
-      case e:SQLException =>
-        Ok(views.html.add("フォームを入力してください。", form))
+    db.withConnection { implicit conn =>
+      val result = SQL("update people set name={name}, mail={mail}, tel={tel} where id = {id}")
+      .on(
+        "name" -> data.name,
+        "mail" -> data.mail,
+        "tel" -> data.tel,
+        "id" -> id
+      ).executeUpdate
+      Redirect(routes.HomeController.index)
     }
-    Redirect(routes.HomeController.index)
   }
 
   def delete(id:Int) = Action{implicit request =>
-    var pdata:Data = null;
-      try{
-        db.withConnection{conn =>
-          val stmt = conn.createStatement
-          val rs = stmt.executeQuery("select * from people where id=" + id)
-          rs.next
-          val name = rs.getString("name")
-          val mail = rs.getString("mail")
-          val tel = rs.getString("tel")
-          pdata = Data(name, mail, tel)
-        }
-      }catch{
-        case e:SQLException =>
-          Redirect(routes.HomeController.index)
-      }
-      Ok(views.html.delete("このレコードを削除します。", pdata, id))
+    db.withConnection { implicit conn =>
+      val data:PersonData = SQL("select * from people where id={id}").on("id" -> id).as(personparser.single)
+      Ok(views.html.delete("このレコードを削除します。", data, id))
+    }
   }
 
   def remove(id:Int) = Action{implicit request =>
-    try
-      db.withConnection{conn =>
-        val ps = conn.prepareStatement("delete from people where id=?")
-        ps.setInt(1, id)
-        ps.executeUpdate
-      }
-    catch{
-      case e: SQLException =>
-        Redirect(routes.HomeController.index)
+    db.withConnection { implicit conn =>
+      val result = SQL("delete from people where id={id}").on("id" -> id).executeUpdate
+      Redirect(routes.HomeController.index)
     }
-    Redirect(routes.HomeController.index)
+  }
+
+  def show(id:Int) = Action{implicit request =>
+    db.withConnection{ implicit conn =>
+      val result:PersonData
+        = SQL("Select * from people where id = {id}").on("id" -> id).as(personparser.single)
+      Ok(views.html.show("People Data.", result))
+    }
   }
 }
--- app/controllers/PersonForm.scala
+++ app/controllers/PersonForm.scala
@@ -1,10 +1,13 @@
 package controllers
 
-object PersonForm{
-  import play.api.data._
-  import play.api.data.Forms._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.db._
+import anorm._
 
+object PersonForm{
   case class Data(name: String, mail: String, tel: String)
+  case class PersonData(id:Int, name:String, mail:String, tel:String)
 
   val form = Form(
     mapping(
@@ -13,4 +16,23 @@ object PersonForm{
       "tel" -> text
     )(Data.apply)(Data.unapply)
   )
+
+  val personform = Form(
+    mapping(
+      "id" -> number,
+      "name" -> text,
+      "mail" -> text,
+      "tel" -> text
+    )(PersonData.apply)(PersonData.unapply)
+  )
+
+  val personparser = {
+    SqlParser.int("people.id") ~
+      SqlParser.str("people.name") ~
+      SqlParser.str("people.mail") ~
+      SqlParser.str("people.tel")
+  }map{
+    case id ~ name ~ mail ~ tel =>
+      PersonData(id, name, mail, tel)
+  }
 }
